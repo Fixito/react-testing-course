596b8bc6a414142038be1ac8110316a8b2aebf91
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-12-11 07:37:23 +0100

    Init

---
+++ 01-rtl-tutorial/src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">React Testing Library</h1>
+      <p className="mt-4 text-gray-700">
+        React Testing Library and Vitest work together to provide a robust
+        testing environment.
+      </p>
+    </div>
+  );
+}
