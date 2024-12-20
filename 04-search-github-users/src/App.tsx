17cda2933bf7df4f7a2a39ff4fce58f3f8f6cbe5
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-12-20 09:12:36 +0100

    Initialize 04-search-github-users project with Vite, React, and TypeScript setup

---
+++ 04-search-github-users/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">App</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
